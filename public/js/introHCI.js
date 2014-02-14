6b338d0362eefda6bc071546cff7007b0016349f
vagrant <vagrant@precise32.(none)>
2014-02-14 09:59:08 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,42 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var clickedOn = '/project/' +idNumber;
+	$.get(clickedOn, addProject);
+
+	console.log("The URL is: " + clickedOn); //do I even need this?
 }
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
+function addProject(result)
+{
+
+	var projectHTML = '<a href="#" class="thumbnail">' +
+'<img src="' + result['image'] + '" class="img">' +
+'<p>' + result['title'] + '</p>' +
+'<p><small>' + result['date'] + '</small></p></a>';
+
+$("#project-container").html(projectHTML);
+$("#project-description").html(result['summary']);
+
+	
+}
+
+function changeColors(result)
+{
+	var colors = result["colors"]["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColors)
 }
